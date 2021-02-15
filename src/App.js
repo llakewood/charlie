e0e43b5de60a40437d5a51dd80c2d551c4f4f4c4
Les Lakewood <les.lakewood@ca.ey.com>
2021-02-15 11:47:04 -0500

    initial timeline commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Chrono } from "react-chrono";
+import { moments } from "./life";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Chrono
+        items={moments}
+        mode="VERTICAL_ALTERNATING"
+        theme={{primary: "#BAC325", secondary: "#2D2723", cardForeColor: "#2D2723" }}
+        />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/life.js
@@ -0,0 +1,40 @@
+export const moments = [{
+		title: "July 2006",
+		cardTitle: "Cabbage Town",
+		cardSubtitle:"River Street Humane Society",
+		cardDetailedText: "A mommy beagle gives birth to a litter with an american husky papa. They were born at the shelter.",
+		media: {
+			type: "IMAGE",
+			source: {
+				url: "/img/charlie_puppy_0.jpg"
+			},
+			name: "Charlie sleeping in a hoodie."
+		}
+	},
+	{
+		title: "August 2006",
+		cardTitle: "Cabbage Town",
+		cardSubtitle:"River Street Humane Society",
+		cardDetailedText: "Sarah and I go to pick out a puppy. It was Charlie, the smallest of her litter.",
+		media: {
+			type: "IMAGE",
+			source: {
+				url: "/img/charlie_puppy_1.jpg"
+			},
+			name: "Charlie in her first days with us."
+		}
+	},
+	{
+		title: "August 2006",
+		cardTitle: "Cabbage Town",
+		cardSubtitle:"River Street Humane Society",
+		cardDetailedText: "As a little baby, she cried in the night. Her mouth was turned into a perfect `O` as I lay outside her crate to show her it was okay to fall asleep. Close your eyes sweet one. We are both new at this.",
+		media: {
+			type: "IMAGE",
+			source: {
+				url: "/img/charlie_puppy_2.jpg"
+			},
+			name: "Charlie in her first days with us."
+		}
+	}
+];
