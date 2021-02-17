406197e72f2dbb107d64eb7bce6c4e1fab45adfe
Les Lakewood <les.lakewood@ca.ey.com>
2021-02-17 10:54:50 -0500

    stories of Riverdale

--- src/life.js
+++ src/life.js
@@ -36,5 +36,31 @@ export const moments = [{
 			},
 			name: "Charlie in her first days with us."
 		}
+	},
+	{
+		title: "September 2006",
+		cardTitle: "Riverdale West",
+		cardSubtitle:"Recall Training",
+		cardDetailedText: "In Cabbagetown, we lived close to the park where we would practice our recall. Ever treat motivated, Charlie would learn to sit and stay while I walked backwarks (eventually forwards) away from her. She would watch and wait for the signal to come running at full speed to my feet for a liver snack. In future antics we will have harrowing experiences in Riverdale that include a) the spring compoost thaw, b) the sex worker, c) the steak sandwich, and d) the Bayview Extension.",
+		media: {
+			type: "IMAGE",
+			source: {
+				url: "/img/riverdale_west.jpg"
+			},
+			name: "The stairs at Riverdale East park leading down into the dog field."
+		}
+	},
+	{
+		title: "June 2007",
+		cardTitle: "Riverdale West",
+		cardSubtitle:"Overheating",
+		cardDetailedText: "As an athlete, we had to learn to temper Charlie's wild bursts in the heat. On Michael's 30th birthday, Charlie got too hot at the park and needed to be carried home when she started to behave drunk. It was around this time that we started to carry water regularly.",
+		media: {
+			type: "IMAGE",
+			source: {
+				url: "/img/riverdale_west.jpg"
+			},
+			name: "The stairs at Riverdale East park leading down into the dog field."
+		}
 	}
 ];
